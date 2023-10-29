3a15a079afeecc38e6fa9bf7a8871008a8f0f9f3
Thindal <thindaltv@gmail.com>
2023-10-29 17:18:16 +0100

    Added persistence and ability to change what responses we give.

--- src/Services/ResponderService/ResponderService.cs
+++ src/Services/ResponderService/ResponderService.cs
@@ -1,16 +1,28 @@
-﻿using Thiccdal.Shared;
+﻿using System.Linq;
+using System.Threading;
+using Thiccdal.Shared;
 using Thiccdal.Shared.EventAggregator;
 using Thiccdal.Shared.Notifications;
 using Thiccdal.Shared.Notifications.Chat;
+using Thiccdal.Shared.Repositories;
 
 namespace ResponderService;
 
+public class Response
+{
+    public string Command { get; set; }
+    public string ResponseText { get; set; }
+}
+
 public class ChatResponderService : IService, IEventSubscriber
 {
-    readonly IEventAggregator _eventAggregator;
-    public ChatResponderService(IEventAggregator eventAggregator)
+    private readonly IEventAggregator _eventAggregator;
+    private readonly IRepository _repository;
+
+    public ChatResponderService(IEventAggregator eventAggregator, IRepository repository)
     {
         _eventAggregator = eventAggregator;
+        _repository = repository;
     }
 
 
@@ -19,36 +31,68 @@ public class ChatResponderService : IService, IEventSubscriber
     {
         _eventAggregator.Subscribe<TwitchChatNotification>(this,
             (msg) => msg.Message.StartsWith("!"),
-            MessageRespondeHandler);
+            MessageHandler);
+
+        _eventAggregator.Subscribe<TwitchChatNotification>(this,
+            (msg) => msg.Message.StartsWith("!resp"),
+            CommandHandler);
+
         return Task.CompletedTask;
     }
 
-    private Task MessageRespondeHandler(TwitchChatNotification msg, CancellationToken cancellationToken)
+    private async Task MessageHandler(TwitchChatNotification msg, CancellationToken cancellationToken)
     {
-        if (string.Equals(msg.Message, "!hi", StringComparison.CurrentCultureIgnoreCase))
-        {
-            if (string.Equals(msg.User.Name, "dukasoft", StringComparison.CurrentCultureIgnoreCase))
-            {                _eventAggregator.Publish(
-                               new OutgoingChatMessage(msg.ChatSource, msg.Channel, "Thiccdal", DateTime.Now, "Hi there Duka you lovely hooman!"));
+        var responses = await _repository.Get<List<Response>>("ChatResponses", msg.Channel, cancellationToken)
+            ?? new List<Response>();
 
-            }
-            else
-            {
-                _eventAggregator.Publish(
-                        new OutgoingChatMessage(msg.ChatSource, msg.Channel, "Thiccdal", DateTime.Now, "Hi there!"));
-            }
-        }
-        else if (
-            (string.Equals(msg.User.Name, "thindal", StringComparison.CurrentCultureIgnoreCase)
-                || string.Equals(msg.User.Name, "dukasoft", StringComparison.CurrentCultureIgnoreCase))
-            && msg.Message.StartsWith("!so ", StringComparison.CurrentCultureIgnoreCase))
+        var responseText = responses.FirstOrDefault(r => r.Command == msg.Message.Substring(1))?.ResponseText;
+        if (responseText != null)
         {
-            var channel = msg.Message.Split(' ')[1];
-            string outMessage = $"Did you know that {channel} streams and are awesome? Because they are!";
-            _eventAggregator.Publish(new OutgoingChatMessage(msg.ChatSource, msg.Channel, "Thindal", DateTime.Now, outMessage));
-
+            await _eventAggregator.Publish(new OutgoingChatMessage(msg.ChatSource, msg.Channel, "Thiccdal", DateTime.Now, responseText), this, cancellationToken);
+            return;
         }
-            return Task.CompletedTask;
+    }
+
+    private async Task CommandHandler(TwitchChatNotification msg, CancellationToken cancellationToken)
+    {
+        var responses = await _repository.Get<List<Response>>("ChatResponses", msg.Channel, cancellationToken)
+            ?? new List<Response>();
+
+        var dukas = new[] { "dukasoft", "thindal" };
+        var dukaName = msg.User.Name.ToLower();
+        if (!dukas.Contains(dukaName))
+            return;
+
+        var parts = msg.Message.Split(' ');
+        if (parts.Length < 3)
+            return;
+        var action = parts[1].ToLower();
+
+        var trigger = parts[2].ToLower();
+        var response = string.Join(' ', parts.Skip(3));
+
+        switch (action)
+        {
+            case "add":
+                responses.Add(new Response() { Command = trigger, ResponseText = response });
+                await _repository.Set("ChatResponses", msg.Channel, responses, cancellationToken);
+                await _eventAggregator.Publish(new OutgoingChatMessage(msg.ChatSource, msg.Channel, "Thiccdal", DateTime.Now, "Added response"), this, cancellationToken);
+                break;
+            case "remove":
+                var toRemove = responses.FirstOrDefault(r => r.Command == trigger);
+                if (toRemove != null)
+                {
+                    responses.Remove(toRemove);
+                    await _repository.Set("ChatResponses", msg.Channel, responses, cancellationToken);
+                    await _eventAggregator.Publish(new OutgoingChatMessage(msg.ChatSource, msg.Channel, "Thiccdal", DateTime.Now, "Removed response"), this, cancellationToken);
+                    break;
+                }
+                await _eventAggregator.Publish(new OutgoingChatMessage(msg.ChatSource, msg.Channel, "Thiccdal", DateTime.Now, "Could not find response"), this, cancellationToken);
+                break;
+            default:
+                await _eventAggregator.Publish(new OutgoingChatMessage(msg.ChatSource, msg.Channel, "Thiccdal", DateTime.Now, "Unknown action"), this, cancellationToken);
+                break;
+        }
     }
 
     public Task Stop()
